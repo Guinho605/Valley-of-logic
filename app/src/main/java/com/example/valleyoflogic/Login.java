45aa1e60a81c23fff6afb58bd8dd28d3fe95ab2f
Eduarda Tiburcio <55164734+TiburcioEduarda@users.noreply.github.com>
2023-07-21 00:10:29 -0300

    Mudanças feitas que estavam no outro.

---
+++ app/src/main/java/com/example/valleyoflogic/Login.java
@@ -0,0 +1,52 @@
+package com.example.valleyoflogic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.valleyoflogic.dao.Dao;
+import com.example.valleyoflogic.model.Usuario;
+
+import java.sql.SQLException;
+
+public class Login extends AppCompatActivity {
+
+    public EditText edtNome, edtIdade;
+    public Button btnIniciar, btnContinue;
+
+    private Dao dao;
+    private Usuario usuario;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        edtNome = findViewById(R.id.txtNome);
+        edtIdade = findViewById(R.id.txtIdade);
+        btnIniciar = findViewById(R.id.btnIniciar);
+        btnContinue = findViewById(R.id.btnContinue);
+
+        dao = new Dao(this);
+    }
+
+    public void limpaEditText() {
+        edtNome.setText(null);
+        edtIdade.setText(null);
+    }
+
+    public void salvar(View view) throws SQLException {
+        Usuario u = new Usuario();
+        u.setNome(edtNome.getText().toString());
+        u.setIdade(Integer.valueOf(edtIdade.getText().toString()));
+        dao = new Dao(this);
+        long id = dao.salvar(u);
+        limpaEditText();
+        //Intent intent = new Intent(getApplicationContext(), MainActivity2.class);
+        //intent.putExtra("nome", usuario.getNome());
+        // startActivity(intent);
+    }
+}
--- app/src/main/java/com/example/valleyoflogic/MainActivity.java
+++ app/src/main/java/com/example/valleyoflogic/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,38 +19,18 @@ import java.sql.SQLException;
 
 public class MainActivity extends AppCompatActivity {
 
-    public EditText edtNome, edtIdade;
-    public Button btnIniciar, btnContinue;
-
-    private Dao dao;
-    private Usuario usuario;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        edtNome = findViewById(R.id.txtNome);
-        edtIdade = findViewById(R.id.txtIdade);
-        btnIniciar = findViewById(R.id.btnIniciar);
-        btnContinue = findViewById(R.id.btnContinue);
 
-        dao = new Dao(this);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Login.class);
+                startActivity(intent);
+            }
+        },1500);
 
-    }
-    public void limpaEditText() {
-        edtNome.setText(null);
-        edtIdade.setText(null);
-    }
-
-    public void salvar(View view) throws SQLException {
-        Usuario u = new Usuario();
-        u.setNome(edtNome.getText().toString());
-        u.setIdade(Integer.valueOf(edtIdade.getText().toString()));
-        dao = new Dao(this);
-        long id = dao.salvar(u);
-        limpaEditText();
-        //Intent intent = new Intent(getApplicationContext(), MainActivity2.class);
-        //intent.putExtra("nome", usuario.getNome());
-       // startActivity(intent);
     }
 }
